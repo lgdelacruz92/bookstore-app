57cce0fade9685d9a7575fd78e08224b418240cb
Lester <lgdelacruz92@gmail.com>
2024-10-03 06:59:44 -0400

    A Okay for now

--- bookstore-ui/src/index.tsx
+++ bookstore-ui/src/index.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import "./services/firebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
