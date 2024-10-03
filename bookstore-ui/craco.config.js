c4c4e74971149b9523e05e90e6496cf629b8c12f
Lester <lgdelacruz92@gmail.com>
2024-10-03 15:44:36 -0400

    docker files

--- bookstore-ui/craco.config.js
+++ bookstore-ui/craco.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
+      "bookstore-shared": "/app/bookstore-shared",
       "@pages": path.resolve(__dirname, "src/pages"),
       "@services": path.resolve(__dirname, "src/services"),
       "@components": path.resolve(__dirname, "src/components"),
