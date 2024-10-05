f48dbad1a82ad6878bcfd109786f2dc58ec70650
Lester <lgdelacruz92@gmail.com>
2024-10-05 10:07:03 -0400

    books list is it's own component

---
+++ bookstore-ui/src/pages/books/components/navigationMenu.tsx
@@ -0,0 +1,12 @@
+import { Flex, Button } from "@radix-ui/themes";
+
+const NavigationMenu = () => {
+  return (
+    <Flex>
+      <Button>Books Library</Button>
+      <Button>Favorites</Button>
+    </Flex>
+  );
+};
+
+export default NavigationMenu;
