cb6fd947f129771a84edc522d581d3c4d1d13ab8
Lester <lgdelacruz92@gmail.com>
2024-10-05 14:28:39 -0400

    add to favorites button

--- bookstore-ui/src/pages/books/components/bookListItem.tsx
+++ bookstore-ui/src/pages/books/components/bookListItem.tsx
@@ -1,4 +1,6 @@
+import { HeartIcon } from "@components/icons/heartIcon";
 import { Book } from "../../../types/book";
+import { Tooltip } from "@radix-ui/themes";
 
 interface BookListItemProps {
   book: Book;
@@ -20,7 +22,12 @@ export const BookListItem = ({ book, ...rest }: BookListItemProps) => {
       className="bg-slate-500 h-[300px] rounded-lg"
       style={backgroundImageStyle}
     >
-      <div className="bg-gray-300 p-2 bg-opacity-30">
+      <div className="bg-gray-300 p-2 bg-opacity-30 relative">
+        <Tooltip content="add to favorites">
+          <div className="absolute top-1 right-1 cursor-pointer">
+            <HeartIcon fill="none" stroke="gray" size="30" />
+          </div>
+        </Tooltip>
         <div className="font-bold text-xl text-slate-800 pt-4">{title}</div>
         <div className="font-bold italic text-slate-700 mt-2">{author}</div>
         <div className="mt-2 pb-2 text-slate-800">{details}</div>
