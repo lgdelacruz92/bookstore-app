fe10344b30249b14333c3d510070d11fc9357e97
Lester <lgdelacruz92@gmail.com>
2024-10-06 09:12:24 -0400

    nav is top on mobile

--- bookstore-ui/src/pages/home/index.tsx
+++ bookstore-ui/src/pages/home/index.tsx
@@ -3,9 +3,12 @@ import SideNav from "./components/sideNav";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen">
-      <div className="py-8 flex-shrink-0 px-4 w-64 border-solid border-r shadow">
+    <div className="flex min-h-screen flex-col lg:flex-row">
+      <div className="hidden lg:block py-8 flex-shrink-0 px-4 w-64 border-solid border-r shadow">
         <SideNav></SideNav>
+      </div>
+      <div className="block lg:hidden py-4 w-full border-b shadow">
+        <SideNav />
       </div>
       <div className="p-8 flex-grow flex-shrink min-w-0">
         <Outlet />
